f5fe1ee1b9203061f72fc30f745ed410b7c856f0
plantplenty <waterbat14@naver.com>
2020-10-08 17:53:05 +0900

    first commit

---
+++ src/main/java/com/pplenty/SharingMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.pplenty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharingMoneyApplication.class, args);
+    }
+
+}
